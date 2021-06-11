3c13e5c4427ebb98de95f44c6480aefe96bee609
Akama4ka <85722901+Akama4ka@users.noreply.github.com>
2021-06-11 07:21:12 +0300

    Create book.h

---
+++ book.h
@@ -0,0 +1,23 @@
+#pragma once
+class book
+{
+
+private:
+	char* author, * title, * publisher;
+	float* price;
+	int* stock;
+public:
+	book() {
+		author = new char[20];
+		title = new char[20];
+		publisher = new char[20];
+		price = new float;
+		stock = new int;
+	}
+	void feeddata();
+	void editdata();
+	void showdata();
+	int search(char[], char[]);
+	void buybook();
+
+};
