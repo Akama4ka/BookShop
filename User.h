569280074c13cf4c1e52dd6be3bac1e3fe1aa118
Akama4ka <85722901+Akama4ka@users.noreply.github.com>
2021-06-16 15:02:16 +0300

    Update User.h

--- User.h
+++ User.h
@@ -1,10 +1,18 @@
-#pragma once
-
+#ifndef USER_H
+#define USER_H
+using namespace std;
 class User
 {
 public:
-
+	string username;
+	string password;
+	User(){
+		username = "clear";
+		password = "clear";
+	}
 	bool LoginandPaasword();
 	void Registor();
-
+	bool searchUser(std::string filePath);
+	~User() {}
 };
+#endif
